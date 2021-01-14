9ba8e4090a7d38b570764dd0e26a8ba649572de6
羡鱼 <dairong@cnhnkj.com>
2021-01-14 15:47:41 +0800

    update

---
+++ index.py
@@ -0,0 +1,22 @@
+#
+# index.py
+# @author bulbasaur
+# @description 
+# @created 2020-05-22T17:30:23.450Z+08:00
+# @last-modified 2020-05-22T18:04:34.785Z+08:00
+#
+
+
+from datetime import datetime
+
+def see_current_time():
+    print(datetime.now())
+
+def SSEEEWAIREDFUNC():
+    SSSSSSS=0
+    return SSSSSSS
+
+
+if __name__ == '__main__':
+    LLLLLLLong_variable = ''
+    see_current_time()
